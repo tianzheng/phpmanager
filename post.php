2bc583fd34d498c712693dbabd56f30dd88b1553
jintianzheng <jintianzheng@tvmining.com>
2016-11-15 22:29:00 +0800

    添加提示框

--- post.php
+++ post.php
@@ -7,12 +7,12 @@ if(!$sql){
 mysql_query("set names 'utf8'");
 mysql_select_db("info_collect",$sql);
 $name="jintian";
-$phone_no="18616951795";
-$email="jintian_2004@163.com";
-$QQ="448414822";
+$phone_no="18616951796";
+$email="jintian_2005@163.com";
+$QQ="448414828";
 $sex="男";
 $exec="insert into info(name,phone_no,email,QQ,sex) value ('$name','$phone_no','$email','$QQ','$sex')";
 mysql_query($exec);
-echo "保存成功";
+echo "<script>alert('信息提交成功');</script>";
 
 ?>
